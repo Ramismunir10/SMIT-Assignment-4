18d6c9ec614e427c6f2afd48e86a3b71b2eac00f
ramismunir10 <ramismunir10@gmail.com>
2024-02-10 01:09:16 +0500

    SMIT Assignment 4

--- Question11.dart
+++ Question11.dart
@@ -8,16 +8,16 @@ The pattern like :
 */
 import 'dart:io';
 
-// void main() {
-//   for (int i = 0; i < 4; i++) {
-//     for (int j = 0; j < 4; j++) {
-//       if (j < i) {
-//         stdout.write('*');
-//       }
-//     }
-//     print('*');
-//   }
-// }
+void main() {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (j < i) {
+        stdout.write('*');
+      }
+    }
+    print('*');
+  }
+}
 
 // void main() {
 //   for (int i = 1; i <= 5; i++) {
